fb9ce59f3f4516dad58e6458211d915a1ffae4c2
narcisoleedev <narcisolee2002@gmail.com>
2024-09-19 14:39:22 -0300

    v1.2

--- main.py
+++ main.py
@@ -2,12 +2,13 @@ import json
 
 from src.verifyIntent import verifyIntent
 from src.extractMessage import iterateIntent
+from src.msgToTxt import msgToTxt
   
 def main():
     with open('./data-json.json', 'r', encoding='utf-8') as file:
         data = json.load(file)
-    #verifyIntent(data)
     iterateIntent(data)
+    msgToTxt()
     
 if __name__ == "__main__":
     main()
--- src/msgToTxt.py
+++ src/msgToTxt.py
@@ -1,10 +1,26 @@
 import pandas as pd
+import re
 
-def treateMessage():
-
+msgs = []
 
 def msgToTxt():
-    df = pd.read_csv('../req.csv', sep='|')
-    for r in df['Message']:
-        treateMessage()
-        
+
+    df = pd.read_csv('./msg.csv', sep='|')
+
+    principal = df[df['Grupo'] == 'principal']  
+    nprincipal = df[df['Grupo'] != 'principal']
+
+    dfOrd = pd.concat([principal, nprincipal])
+
+    for i, r in dfOrd.iterrows(): #Itera sobre o DataFrame
+        if r['Message'] is not None: #Se a mensagem não for vazia ele adiciona ao array de msgs
+            if(r['TipoMsg']=='payload'): #Se for payload ele faz o tratamento e adiciona a msgs
+                match = re.search(r"'(.*?)'", r['Message'])
+                if(match!=''): #Se não retornar nada 
+                    msgs.append(match)
+            else: #Adiciona as messages ao array
+                msgs.append(r['Message'])
+    with open('./msg.txt', 'w') as f:
+        for msg in msgs:
+            f.write(msg + '\n') 
+
